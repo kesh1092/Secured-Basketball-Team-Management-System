a483b13df32c6e6925a64934a5996194ed46fdc4
Kousei Richeson <kousei@nmsu.edu>
2017-11-05 22:16:16 -0700

    Fixed Phone Number Bug

--- PlayerSystem/PlayerPage.php
+++ PlayerSystem/PlayerPage.php
@@ -135,7 +135,7 @@ require_once('SQLFunctions.php');
     
     <p>
       <label>Phone Number</label>
-      <input type="number" name="PhoneNumber" value="<?php echo $PhoneNumber;?>" minlength="10" maxlength="10" required/>
+      <input type="number" name="PhoneNumber" value="<?php echo $PhoneNumber;?>" min="1000000000" max="9999999999" required/>
       <i>(10 characters, no dashes or parentheses)</i>
     </p>
     <p> 
